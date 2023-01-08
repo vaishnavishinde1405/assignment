19482981761e972e3f27945b9a7bf5b1d6c01ded
vaishnavishinde1405 <20104002.vaishnavishinde@gmail.com>
2023-01-08 20:58:58 +0530

    the assignment

---
+++ src/Config/Fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const config = {
+    apiKey: "AIzaSyAaCElud0Ife0vkits2BScONMaX_nw--As",
+    authDomain: "vicode-media.firebaseapp.com",
+    projectId: "vicode-media",
+    storageBucket: "vicode-media.appspot.com",
+    messagingSenderId: "420658677235",
+    appId: "1:420658677235:web:eac7e5990ee0ebe38b7e86"
+};
+
+const fire = firebase.initializeApp(config);
+export default fire;
